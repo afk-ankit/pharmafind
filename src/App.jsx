34d3eed305704f7938121a2ac308333dc79c8b48
Ankit Sharma <ankitsharmagh093@gmail.com>
2024-12-15 04:02:51 +0530

    added react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,38 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home';
-import Login from './Pages/Login';
-import './App.css';
-import "slick-carousel/slick/slick.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import "./App.css";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
 
-import HeroSection from './components/HeroSection';
-import SearchResults from './Pages/SearchResults';
+import HeroSection from "./components/HeroSection";
+import SearchResults from "./Pages/SearchResults";
 
-import Register from './Pages/Register';
+import Register from "./Pages/Register";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
 
-        <Route path="/" element={<HeroSection />} />
-        <Route path="/search" element={<SearchResults />} />
+          <Route path="/" element={<HeroSection />} />
+          <Route path="/search" element={<SearchResults />} />
 
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
 
-        <Route path="/home" element={<Home />} />
-
-
-
-      </Routes>
-    </Router>
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 };
 
 export default App;
-
-
